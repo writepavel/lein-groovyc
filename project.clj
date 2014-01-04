0e4d55468e082b75420fbc6c99fd228121169048
writepavel <writepavel@gmail.com>
2014-01-04 03:36:12 +0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-groovyc "0.2.0"
+(defproject lein-groovyc "0.3.0"
   :description "Compile groovy sources"
   :dependencies [[org.codehaus.groovy/groovy-all "2.2.1" ] [lancet "1.0.2"]]
   :eval-in-leinigen true)
